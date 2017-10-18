f882cc46c3344a655ed0f97b7dbb76583da3a0db
phodina <hodinapetr46@gmail.com>
2017-10-18 13:09:06 +0200

    Implemented error logic for arg parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
 	
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 	
     println!("In file {}", config.filename);
 
@@ -25,16 +29,16 @@ struct Config {
 }
 
 impl Config {
-	fn new(args: &[String]) -> Config {
+	fn new(args: &[String]) -> Result<Config, &'static str> {
 		
 		if args.len() < 3 {
 			
-			panic!("Not enough arguments");
+			return Err("Not enough arguments");
 		}
 		
 		let query = args[1].clone();
 		let filename = args[2].clone();
 		
-		Config {query, filename}
+		Ok(Config {query, filename})
 		}
 }
