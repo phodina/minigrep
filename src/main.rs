15667d783efb8cb47b1045f12b71dc177dce4452
phodina <hodinapetr46@gmail.com>
2017-10-18 13:19:13 +0200

    Program logic refactored under run function & handles now all errors.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 fn main() {
 
@@ -12,15 +13,27 @@ fn main() {
         process::exit(1);
     });
 	
+	println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+	if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+	
+    
+}
+
+fn run (config:Config) -> Result<(), Box<Error>> {
+
+	let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     println!("With text:\n{}", contents);
+	
+	Ok(())
 }
 
 struct Config {
