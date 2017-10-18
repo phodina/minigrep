7da3e76e598fd86171ca6f1c5cd81ce47905f141
phodina <hodinapetr46@gmail.com>
2017-10-18 12:58:43 +0200

    Parsing of args moved into struct implementation.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 	
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 	
     println!("In file {}", config.filename);
 
@@ -24,10 +24,17 @@ struct Config {
 	filename: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-
-	let query = args[1].clone();
-    let filename = args[2].clone();
-	
-	Config {query, filename}
+impl Config {
+	fn new(args: &[String]) -> Config {
+		
+		if args.len() < 3 {
+			
+			panic!("Not enough arguments");
+		}
+		
+		let query = args[1].clone();
+		let filename = args[2].clone();
+		
+		Config {query, filename}
+		}
 }
