0a54ce8e1c5aef5cb02004262ab266696d3d9e07
phodina <hodinapetr46@gmail.com>
2017-10-18 12:49:06 +0200

    Refactor parsed args into struct instead of tuple.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 	
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 	
-    println!("In file {}", filename);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -19,10 +19,15 @@ fn main() {
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
+struct Config {
+	query: String,
+	filename: String
+}
 
-	let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+
+	let query = args[1].clone();
+    let filename = args[2].clone();
 	
-	(query, filename)
+	Config {query, filename}
 }
