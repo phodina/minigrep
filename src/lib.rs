ea10e965eb3195fddb0ac4cc9204550169770e91
phodina <hodinapetr46@gmail.com>
2017-10-19 13:33:22 +0200

    Refactored arg parsing using iterators.

--- src/lib.rs
+++ src/lib.rs
@@ -30,19 +30,25 @@ pub struct Config {
 }
 
 impl Config {
-	pub fn new(args: &[String]) -> Result<Config, &'static str> {
+	pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
 		
-		if args.len() < 3 {
-			
-			return Err("Not enough arguments");
-		}
+		args.next();
 		
-		let query = args[1].clone();
-		let filename = args[2].clone();
-		
-		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+		let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
 
-        Ok(Config { query, filename, case_sensitive })
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query, filename, case_sensitive
+        })
 		}
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
